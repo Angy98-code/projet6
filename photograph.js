b8cb62210b4edc9ad5d44160e69d0c088e711e58
Angy98-code <81024074+Angy98-code@users.noreply.github.com>
2021-08-08 19:15:10 +0200

    page acceuil ok recherches sur les paramètres d'URL et sur comment les manipuler.

--- photograph.js
+++ photograph.js
@@ -1,194 +1,95 @@
-//
-// HEADER NAV ARRAY
-//
-/*
-const navArray = [
-  { Portrait: "#Portrait" },
-  { Art: "#Art" },
-  { Fashion: "#Fashion" },
-  { Architecture: "#Architecture" },
-  { Travel: "#Travel" },
-  { Sport: "#Sport" },
-  { Animals: "#Animals" },
-  { Event: "#Event" },
-];
-const getBlockNav = (
-  Portrait,
-  Art,
-  Fashion,
-  Architecture,
-  Travel,
-  Sport,
-  Animals,
-  Events
-) => `
-<div id="list_nav">
-    <ul>
-    <a href="">${Portrait}</a>
-    <a href="">${Art}</a>
-<a href="">${Fashion}</a>
-    <a href="">${Architecture}</a>
-     <a href="">${Travel}</a>
-    <a href="">${Sport}</a>
-<a href="">${Animals}</a>
-    <a href="">${Events}</a>
-    
-    </ul>
-
-    </div>
-`;
-
-/*
-class Photograph {
-    getBlock(){
-        return `
-<div>
-    <h1>${name}</h1>
-    <div>${town}</div>
-
-</div>
-`;
-    }
-}
-
-
-const elementNav = document.getElementById("list_nav");
-elementNav.innerHTML = navArray.map((nav) => {
-  // nav.preventDefault();
-  return getBlockNav(
-    nav.Portrait,
-    nav.Art,
-    nav.Fashion,
-    nav.Architecture,
-    nav.Travel,
-    nav.Sport,
-    nav.Animals,
-    nav.Events
-  );
-});
-*/
-//{ name: "Mimi Keel", town: "London UK" },
-//{ name: "Mary Ton", city: "London UK" },
-
-//
+// PAGE D'ACCUEIL
 // SECTION PHOTOGRAPH ARRAY
 //
 const photographArray = [
   {
+    portrait: "MimiKeel.jpg.jpg",
     name: "Mimi Keel",
     id: 243,
     city: "London",
     country: "UK",
-    tags: ["portrait", "events", "travel", "animals"],
     tagline: "Voir le beau dans le quotidien",
     price: 400,
-    portrait: "MimiKeel.jpg.jpg",
+    tags: ["portrait", "events", "travel", "animals"],
   },
 
   {
+    portrait: "EllieRoseWilkens.jpg.jpg",
     name: "Ellie-Rose Wilkens",
     id: 930,
     city: "Paris",
     country: "France",
-    tags: ["sports", "architecture"],
     tagline: "Capturer des compositions complexes",
     price: 250,
-    portrait: "EllieRoseWilkens.jpg",
+    tags: ["sports", "architecture"],
   },
   {
+    portrait: "TracyGalindo.jpg.jpg",
     name: "Tracy Galindo",
     id: 82,
     city: "Montreal",
     country: "Canada",
-    tags: ["art", "fashion", "events"],
     tagline: "Photographe freelance",
     price: 500,
-    portrait: "TracyGalindo.jpg",
+    tags: ["art", "fashion", "events"],
   },
   {
+    portrait: "NabeelBradford.jpg.jpg",
     name: "Nabeel Bradford",
     id: 527,
     city: "Mexico City",
     country: "Mexico",
-    tags: ["travel", "portrait"],
     tagline: "Toujours aller de l'avant",
     price: 350,
-    portrait: "NabeelBradford.jpg",
+    tags: ["travel", "portrait"],
   },
   {
+    portrait: "RhodeDubois.jpg.jpg",
     name: "Rhode Dubois",
     id: 925,
     city: "Barcelona",
     country: "Spain",
-    tags: ["sport", "fashion", "events", "animals"],
     tagline: "Je crée des souvenirs",
     price: 275,
-    portrait: "RhodeDubois.jpg",
+    tags: ["sport", "fashion", "events", "animals"],
   },
   {
+    portrait: "MarcelNikolic.jpg.jpg",
     name: "Marcel Nikolic",
     id: 195,
     city: "Berlin",
     country: "Germany",
-    tags: ["travel", "architecture"],
     tagline: "Toujours à la recherche de LA photo",
     price: 300,
-    portrait: "MarcelNikolic.jpg",
+    tags: ["travel", "architecture"],
   },
 ];
-const getBlock = (name, id, city, country, tags, tagline, price, portrait) => `
-<div id="list_container">
-    <h2 class="name">${name}</h2>
+
+const getBlock = (portrait, name, id, city, country, tagline, price, tags) => `
+<div id="list_container1">
+<a href="/photograph" class="portrait"><img src="images/${portrait}"/></a>        
+<h2 class="name">${name}</h2>
     <template class="id">${id}</template>
-    <div class="city">${city}</div>
-    <div class="country">${country}</div>
-    <div class="tags">${tags}</div>
+    <div class="city_country">${city}, ${country}</div>
     <div class="tagline">${tagline}</div>
-    <div class="price">${price}</div>
-    <div class="portrait">${portrait}</div>
+    <div class="price">${price}€/jour</div>
+   <ul class="container_tags">
+                    <li><a href="index.html" class="tags">#${tags}</a></li>
+                </ul>
+                
     </div>
-`;
+    `;
 
-var nouvelleimage = document.createElement("IMG");
-document.getElementById("portrait").appendChild(nouvelleimage);
-document
-  .getElementById("portrait")
-  .lastChild.setAttribute("src", MimiKeel.jpg.jpg);
-
-/*
-class Photograph {
-    getBlock(){
-        return `
-<div>
-    <h1>${name}</h1>
-    <div>${town}</div>
-
-</div>
-`;
-    }
-}
-*/
 const element = document.getElementById("list_container");
 element.innerHTML = photographArray.map((photograph) => {
   return getBlock(
+    photograph.portrait,
     photograph.name,
     photograph.id,
     photograph.city,
     photograph.country,
-    photograph.tags,
     photograph.tagline,
     photograph.price,
-    photograph.portrait
+    photograph.tags
   );
 });
-//
-//pour les images des photographes NE FONCTIONNE PAS
-//
-//
-/*
-let imgMimiKeel = document.createElement("imgMimiKeel");
-img.src = "MimiKeel.jpg.jpg";
-
-let photoPortrait = document.getElementById("portrait");
-photoPortrait.appendChild(img);*/
-//block.setAttribute("style", "text-align:center");
