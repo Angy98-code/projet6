deaa1414f61aca464d441b49023a5a2f8cea99bb
Angy98-code <81024074+Angy98-code@users.noreply.github.com>
2021-12-06 12:04:08 +0100

    version finale

--- js2.js
+++ js2.js
@@ -663,12 +663,12 @@ document
       messageValidation.style.display = "block";
       const form = document.getElementById("inscription");
       form.style.display = "none";
-      console.log({
-        prénom: "isFirstNameValid",
-        nom: "isLastNameValid",
-        email: "isEmailValid",
-        message: "isMessageValid",
-      });
+      // console.log({
+      //   prénom: "isFirstNameValid",
+      //   nom: "isLastNameValid",
+      //   email: "isEmailValid",
+      //   message: "isMessageValid",
+      // });
     }
   });
 //
